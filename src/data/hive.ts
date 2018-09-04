a39c74884b6155ba528aa90f95e88bf99876f603
lleohao <lleohao@hotmail.com>
2018-09-04 13:55:45 +0800

    feat: Generate keyword data
    keyword data from @mammut-fe/hive-data
    
    close #1

---
+++ src/data/hive.ts
@@ -0,0 +1,26 @@
+import {builtInFunctions, keywords} from '@mammut-fe/hive-data';
+
+export const data: any = {
+  'use': [
+    {
+      name: 'use',
+      desc: 'USE sets the current database for all subsequent HiveQL statements.',
+      restriction: 'string',
+      values: [
+        {
+          name: 'DEFAULT',
+          desc: 'HiveQL default database.'
+        }
+      ]
+    }
+  ],
+  'select': [
+    {
+      name: 'select'
+    }
+  ],
+  builtInFunctions,
+  keywords
+};
+
+export const descriptions: any = {};
